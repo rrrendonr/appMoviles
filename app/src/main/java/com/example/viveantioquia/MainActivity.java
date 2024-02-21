3b1ca3672958003d263f8cf24f7641001de068e3
rrendon <riky2535@gmail.com>
2024-02-20 23:58:52 -0500

    Navegacion a menu activity

--- app/src/main/java/com/example/viveantioquia/MainActivity.java
+++ app/src/main/java/com/example/viveantioquia/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.viveantioquia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView btn_urrao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_urrao = findViewById(R.id.card_urrao_btn);
+        btn_urrao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
